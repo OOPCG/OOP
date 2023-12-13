8106ee6e86f2c1c77cfb3fe59cf48382db101df3
OOPCG <153728949+OOPCG@users.noreply.github.com>
2023-12-13 22:12:08 +0530

    Update Assignment2.cpp

--- SYCOB30OOP/Assignment2/Assignment2.cpp
+++ SYCOB30OOP/Assignment2/Assignment2.cpp
@@ -1,120 +1,168 @@
-#include <iostream>
+/*
+Experiment Number 2 : Develop a program in C++ to create a database of
+student’s information system
+containing the following information: Name, Roll number, Class, Division,
+Date of Birth, Blood group,
+Contactaddress, Telephone number, Driving license no. and other. Construct
+the database with
+suitable member functions. Make use of constructor, default constructor,
+copy constructor,
+destructor, static member functions, friend class, this pointer, inline
+code and dynamic
+memory allocation operators-new and delete as well as exception handling.
+*/
+
+
+
+#include<iostream>
+#include<string.h>
 using namespace std;
-class data
-{
-private:
-string name;
-char bg;
-string address;
-string dob;
-string cl;
-string div;
-string lic;
-static int c;
-int roll;
-long int phone;
-public:
-data();
-static int getcount();
-void getdata();
-void show();
-data(data *obj);
-data(int roll,long int phone,string name,string address,string dob,string cl,string div, string lic);
-~data();
+
+class StudData;
+
+class Student{
+    string name;
+    int roll_no;
+    string cls;
+    char* division;
+    string dob;
+    char* bloodgroup;
+    static int count;
+
+    public:
+
+    Student()          // Default Constructor
+    {
+        name="";
+        roll_no=0;
+        cls="";
+        division=new char;
+        dob="dd/mm/yyyy";
+        bloodgroup=new char[4];
+    }
+
+    ~Student()
+    {
+        delete division;
+        delete[] bloodgroup;
+    }
+
+    static int getCount()
+    {
+        return count;
+    }
+
+    void getData(StudData*);
+    void dispData(StudData*);
 };
-int data::c=0;
-data::data(data *obj)
-{
-cout<<"\nCopy constructor implemented"<<endl;
-}
-data::~data()
-{
-cout<<"Destructor called \n";
-}
-void data::getdata()
-{
-cout<<"Enter roll number \n";
-cin>>roll;
-cout<<"Enter telephone number \n";
-cin>>phone;
-cout<<"Enter Name \n";
-cin>>name;
-cout<<"Enter address \n";
-cin>>address;
-cout<<"Enter D.O.B \n";
-cin>>dob;
-cout<<"Enter Class \n";
-cin>>cl;
-cout<<"Enter Diviosn \n";
-cin>>div;
 
-cout<<"Enter license number \n";
-cin>>lic;
-}
-void data::show()
-{
-cout<<"Name :"<<name<<endl;
-cout<<"Roll Number :"<<roll<<endl;
-cout<<"Telephone Number :"<<phone<<endl;
-cout<<"Address :"<<address<<endl;
-cout<<"Date of birth :"<<dob<<endl;
-cout<<"Class :"<<cl<<endl;
-cout<<"Division :"<<div<<endl;
-cout<<"License number :"<<lic<<endl;
-}
-data::data(int roll,long int phone,string name,string address,string dob,string cl,string div,string lic)
-{
-cout<<"\nParameterized Constructor"<<endl;
-c++;
-this->roll=roll;
+class StudData{
+    string caddress;
+    long int* telno;
+    long int* dlno;
+    friend class Student;
 
-this->phone=phone;
-this->name=name;
-this->address=address;
-this->dob=dob;
-this->div=div;
-this->div=div;
-this->lic=lic;
-}
-data::data()
+    public:
+
+    StudData()
+    {
+        caddress="";
+        telno=new long;
+        dlno=new long;
+    }
+    
+    ~StudData()
+    {
+        delete telno;
+        delete dlno;
+    }
+
+    void getStudData()
+    {
+        cout<<"Enter Contact Address : ";
+        cin.get();
+        getline(cin,caddress);
+        cout<<"Enter Telephone Number : ";
+        cin>>*telno;
+        cout<<"Enter Driving License Number : ";
+        cin>>*dlno;
+    }
+
+    void dispStudData()
+    {
+        cout<<"Contact Address : "<<caddress<<endl;
+        cout<<"Telephone Number : "<<telno<<endl;
+        cout<<"Driving License Number : "<<dlno<<endl;
+    }
+};
+
+inline void Student::getData(StudData* st)
 {
-roll=0;
-phone=0;
-name="Name";
-address="Address";
-dob="DOB";
-cl="Class";
-div="Division";
-lic="License no.";
-cout<<"Default Constructor"<<endl;
-c++;
+    cout<<"Enter Student Name : ";
+    getline(cin,name);
+    cout<<"Enter Roll Number : ";
+    cin>>roll_no;
+    cout<<"Enter Class : ";
+    cin.get();
+    getline(cin,cls);
+    cout<<"Enter Division : ";
+    cin>>division;
+    cout<<"Enter Date of Birth : ";
+    cin.get();
+    getline(cin,dob);
+    cout<<"Enter Blood Group : ";
+    cin>>bloodgroup;
+    st->getStudData();
+    count++;
 }
-int data::getcount()
+
+inline void Student::dispData(StudData* st1)
 {
-return c;
+    cout<<"Student Name : "<<name<<endl;
+    cout<<"Roll Number : "<<roll_no<<endl;
+    cout<<"Class : "<<cls<<endl;
+    cout<<"Division : "<<division<<endl;
+    cout<<"Date of Birth : "<<dob<<endl;
+    cout<<"Blood Group : "<<bloodgroup<<endl;
+    st1->dispStudData();
 }
+
+int Student::count;
+
 int main()
 {
-int num;
-data *d1= new data();
-d1->show();
-delete d1;
-data *d2=new data(73,9823694961,"Vijay","Ravet","26.11.85","SE"," A","MSIN85U");
-d2->show();
-data *d3=new data(d2);
-d3->show();
-delete d2;
-cout<<"\nEnter size of database";
-cin>>num;
-data dx[num];
-for(int i=0;i<num;i++)
-{
-dx[i].getdata();
-}
-for(int i=0;i<num;i++)
-{
-dx[i].show();
-}
-cout<<"Number of constructor calls and total number of objects:"<<data::getcount()<<endl;
-return 0;
+    Student* stud1[100];
+    StudData* stud2[100];
+    int n=0;
+    char ch;
+
+    do
+    {
+        stud1[n]=new Student;
+        stud2[n]=new StudData;
+        stud1[n]->getData(stud2[n]);
+        n++;
+        cout<<"Do you want to add another student (y/n) : ";
+        cin>>ch;
+        cin.get();
+    } while (ch=='y' || ch=='Y');
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<"---------------------------------------------------------------"<<endl;
+        stud1[i]->dispData(stud2[i]);
+    }
+
+    cout<<"---------------------------------------------------------------"<<endl;
+    cout<<"Total Students : "<<Student::getCount();
+    cout<<endl<<"---------------------------------------------------------------"<<endl;
+
+    for(int i=0;i<n;i++)
+    {
+        delete stud1[i];
+        delete stud2[i];
+    }
+    
+
+    return 0;
 }
